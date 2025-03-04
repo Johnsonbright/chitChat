7f8448ca9f6e4f6e66a8bae016373b7a2549dc04
Ifechukwu Johnson Uzoma <johnsonbright101@gmail.com>
2025-03-04 19:49:36 +0100

    generate apk file for my project

--- android/app/src/main/java/com/uzoma333/chitChat/MainActivity.kt
+++ android/app/src/main/java/com/uzoma333/chitChat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.uzoma333.chitChat
+package com.uzoma333.chitchat
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/uzoma333/chitChat/MainApplication.kt
+++ android/app/src/main/java/com/uzoma333/chitChat/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.uzoma333.chitChat
+package com.uzoma333.chitchat
 
 import android.app.Application
 import android.content.res.Configuration
